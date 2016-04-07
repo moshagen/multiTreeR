73122847aa0d348d6704afd6c13d1d04051692a6
moshagen <moshagen@uni-kassel.dfe>
2016-04-07 17:07:01 +0200

    demo

--- demo/demo1.R
+++ demo/demo1.R
@@ -4,5 +4,6 @@ fMdt <- system.file("extdata", "source.mdt", package="multiTreeR")
 
 # fit model using restrictions a=g and D1=D2
 res <- doMT(fEqn, fMdt, restrictions = list('a=g','D1=D2'))
+
 # show results
 summary(res)
