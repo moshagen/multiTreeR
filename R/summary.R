f365d32426fe0b73f46e26c31125ad53f5b5b25e
moshagen <moshagen@uni-kassel.dfe>
2017-06-15 19:35:13 +0200

    fix bug concerning mean parameters in summary

--- R/summary.R
+++ R/summary.R
@@ -31,7 +31,7 @@ summary.multiTreeR <- function(mtres, digits=4, ci=FALSE){
   }
 
   # prep aggr param
-  pagg <-t(apply(res$paramEst, 2, FUN=function(x){c(mean(x),sd(x),quantile(x, c(.025, .50, .975)))}))
+  pagg <-t(apply(mtres$paramEst, 2, FUN=function(x){c(mean(x),sd(x),quantile(x, c(.025, .50, .975)))}))
   colnames(pagg) <- c('Mean', 'SD', '2.5%', 'Median', '97.5%')
 
   cat("\nModel Fit:\n\n")
